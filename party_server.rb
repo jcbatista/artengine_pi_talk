31e86009cff238811ff7b6f44cf56cadf69ba86e
Jean-Claude Batista <jeanclaudebatista@gmail.com>
2014-06-27 18:09:27 +0000

    getting ip, port info from Sinatra

--- party_server.rb
+++ party_server.rb
@@ -1,11 +1,19 @@
 require 'rubygems'
 require 'sinatra'
+require 'socket'
 require 'json'
-
 require_relative 'party.rb' 
 
 relay_gpio = 7
 set :bind, '0.0.0.0' # allow users to connect to the server
+
+def get_public_ipv4
+  # Socket.ip_address_list.detect{|intf| intf.ipv4? and !intf.ipv4_loopback? and !intf.ipv4_multicast? and intf.ipv4_private?}
+  Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+end
+
+my_ip = get_public_ipv4().ip_address()
+puts "Sinatra running at ip=#{my_ip}, port=#{settings.port}"
 
 get '/' do
   File.read(File.join('public', 'index.html'))
